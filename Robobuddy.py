196237fc10a7d89699a55d4807c97d519584df56
Kumari Ayushi <143933979+KumariAyushi10@users.noreply.github.com>
2024-12-21 14:19:58 +0530

    Add files via upload

---
+++ Robobuddy.py
@@ -0,0 +1,121 @@
+import pyttsx3 
+import speech_recognition as spr 
+import datetime
+import wikipedia
+import webbrowser
+import os
+import smtplib
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+# print(voices[1].id)
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")   
+
+    else:
+        speak("Good Evening!")  
+
+    speak("I am Robobuddy . Please tell me how may I help you")
+def takeCommand():
+    r = spr.Recognizer()
+    with spr.Microphone() as source:
+        ("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        # print(e)    
+        speak("Say that again please...")  
+        return "None"
+    return query
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login(email_id, password)
+    server.sendmail(email_id, to, content)
+    server.close()
+
+if __name__ == "__main__":
+    wishMe()
+    while True:
+    # if 1:
+        query = takeCommand().lower()
+
+        # Logic for executing tasks based on query
+        if 'search' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+        
+        elif 'what' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")  
+
+
+        elif 'song' in query:
+            webbrowser.open("https://www.youtube.com/watch?v=JGwWNGJdvx8") 
+
+        elif 'music' in query:
+            webbrowser.open("https://www.youtube.com/watch?v=JGwWNGJdvx8") 
+
+        elif 'time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            speak(f"Sir, the time is {strTime}")
+
+        elif 'open code' in query:
+            codePath = "C:\\Users\\Haris\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            os.startfile(codePath)
+
+        elif 'email to ayushi' in query:
+            try:
+                speak('What is your email id')
+                email_id = input("Your email -> ")
+                speak('What is password')
+                password = input("Password -> ")
+                speak("What is the receiver's email")
+                remail_id = input("Receiver's email -> ")
+                speak("What should I say?")
+                content = takeCommand()
+                to = remail_id  
+                sendEmail(to, content)
+                speak("Email has been sent!")
+            except Exception as e:
+                print(e)
+                speak("Sorry . I am not able to send this email")    
+
